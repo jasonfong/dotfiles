0e3f6ae5062ce4eb1152a6bf88a59c466be8ae50
Fong, Jason <Jason.G.Fong@disney.com>
2016-06-08 20:14:49 -0700

    add path for GAE Python SDK when using Cloud SDK installation

--- zsh/gcloud.zsh
+++ zsh/gcloud.zsh
@@ -6,5 +6,8 @@ source "${GCLOUD_BASE}/path.zsh.inc"
 # Enable shell command completion for gcloud.
 source "${GCLOUD_BASE}/completion.zsh.inc"
 
+# Add paths for GAE Python SDK
+export PATH=${GCLOUD_BASE}/platform/google_appengine:${PATH}
+
 # Google App Engine Python libraries
 export PYTHONPATH="${PYTHONPATH}:${APPENGINE_BASE}:${APPENGINE_BASE}/lib/yaml/lib:${APPENGINE_BASE}/lib/fancy_urllib:./lib"
